008d657df03d6fa233910fa602369e6132291942
N8BWert <n8.wert.b@gmail.com>
2022-11-12 18:21:14 -0500

    Initial Commit

---
+++ Shared/sandwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandwichApp.swift
+//  Shared
+//
+//  Created by nate wert on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct sandwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
